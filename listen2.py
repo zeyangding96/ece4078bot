d84f4c85772122a1524d2a2581adfe67a177b0b9
Ding Ze Yang <zeyangding96@gmail.com>
2026-04-23 15:16:29 +0800

    Update listen2.py

--- listen2.py
+++ listen2.py
@@ -273,7 +273,7 @@ def pid_control():
         final_right_pwm = apply_min_threshold(ramp_right_pwm, MIN_PWM_THRESHOLD)
         set_motors(final_left_pwm, final_right_pwm)
         
-        if ramp_left_pwm !=  and args.verbose: # print pwm values for debugging purpose
+        if ramp_left_pwm != 0 and args.verbose: # print pwm values for debugging purpose
             print(f"L/R PWM: ({ramp_left_pwm:.2f},{ramp_right_pwm:.2f}), L/R Enc: ({left_count}, {right_count})")
         
         time.sleep(0.01)
